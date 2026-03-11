8c905b8c2c21b79ed895d9ef9075ae8133e52f92
ludrosse <144143227+ludrosseingetis@users.noreply.github.com>
2026-03-11 15:30:44 +0100

    Create runner.py

---
+++ tester/runner.py
@@ -0,0 +1,30 @@
+import requests
+import time
+import statistics
+import storage # Importe le fichier storage.py au dessus
+
+def run_tests():
+    url = "https://api.frankfurter.app/latest?from=EUR&to=USD"
+    latencies = []
+    passed = 0
+    
+    for i in range(5): # On fait 5 tests pour la QoS
+        try:
+            start = time.perf_counter()
+            r = requests.get(url, timeout=3)
+            lat = (time.perf_counter() - start) * 1000
+            latencies.append(lat)
+            
+            if r.status_code == 200 and "rates" in r.json():
+                passed += 1
+        except:
+            pass
+
+    success_rate = (passed / 5) * 100
+    avg_lat = statistics.mean(latencies) if latencies else 0
+    
+    # On enregistre dans la BDD
+    storage.save_run(success_rate, avg_lat, "Test automatique Frankfurter")
+
+if __name__ == "__main__":
+    run_tests()
