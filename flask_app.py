6d75677551347d63bb5a261d7d806de00529d8ec
ludrosse <144143227+ludrosseingetis@users.noreply.github.com>
2026-03-11 15:23:50 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -6,29 +6,11 @@ from werkzeug.utils import secure_filename
 import sqlite3
 
 app = Flask(__name__)
-@app.get("/")
 
+@app.get("/")
 def consignes():
      return render_template('consignes.html')
-with app.app_context():
-    storage.init_db()
-     
-@app.route("/dashboard")
-def dashboard():
-    # On récupère les 10 derniers runs depuis SQLite
-    runs = storage.get_runs()
-    return render_template('dashboard.html', runs=runs)
-
-@app.route("/run")
-def trigger_test():
-    # Cette route permet de lancer le test via un bouton sur le dashboard
-    from tester import runner
-    runner.run_tests()
-    return redirect(url_for('dashboard'))
-
 
 if __name__ == "__main__":
-
     # utile en local uniquement
-
     app.run(host="0.0.0.0", port=5000, debug=True)
